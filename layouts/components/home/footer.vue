7918d8d6e37c226e20d81dc5bf2ba707c51dc1cf
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-05-05 19:44:16 +0700

    Fix: Fill the footer content

--- layouts/components/home/footer.vue
+++ layouts/components/home/footer.vue
@@ -4,13 +4,56 @@
 		<div class="footer-top">
 			<div class="container-small">
 				<div class="row">
-					<div class="col-md-5">
+					<div class="mb-5 mb-lg-0 col-md-5">
 						<router-link class="text-decoration-none d-flex mb-3" to="/">
 							<img :src="imageLogo" alt="logo" height="96" width="auto" />
 						</router-link>
 
-            <p class="text-white">Museum Trinil, merupakan sebuah museum yang berada di Kawu, Kedunggalar, Ngawi yang memberikan wawasan mengenai kehidupan, kebudayaan dan juga keberadaban manusia pada zaman dahulu di Indonesia.</p>
+						<p class="text-white mb-0">
+							Museum Trinil, merupakan sebuah museum yang berada di Kawu,
+							Kedunggalar, Ngawi yang memberikan wawasan mengenai kehidupan,
+							kebudayaan dan juga keberadaban manusia pada zaman dahulu di
+							Indonesia.
+						</p>
 					</div>
+
+					<div class="mb-5 mb-lg-0 col-md-3">
+						<h3 class="text-white mb-4">Hubungi</h3>
+						<p class="text-white opacity-75">
+							<strong>Sekretariat Museum Trinil</strong><br />Pilang, Kawu, Kec. Kedunggalar,
+							Kabupaten Ngawi, Jawa Timur 63254
+						</p>
+						<div>
+							<a
+								href="https://wa.me/6285735477211"
+								class="text-white opacity-75 mb-3 d-flex gap-2 text-decoration-none align-items-center"
+							>
+								<i class="fab fa-whatsapp"></i>
+								<span>+62 857-3547-7211</span>
+							</a>
+						</div>
+						<div>
+							<a
+								href="mailto:sekretariat@trinil.ngawikab.go.id"
+								class="text-white opacity-75 d-flex gap-2 text-decoration-none align-items-center"
+							>
+								<i class="fas fa-envelope"></i>
+								<span>sekretariat@trinil.ngawikab.go.id</span>
+							</a>
+						</div>
+					</div>
+
+					<div class="mb-5 mb-lg-0 col-md-2">
+						<h3 class="text-white mb-4">Tautan Lain</h3>
+						<ul class="list-unstyled d-flex flex-column gap-3">
+              <li v-for="(item, index) in otherLinks" :key="index">
+                <router-link class="text-white text-decoration-none opacity-75" :to="item.link">{{ item.name }}</router-link>
+              </li>
+            </ul>
+					</div>
+          <div class="mb-5 mb-lg-0 col-md-2">
+            <nuxt-img src="/images/lets-encrypt.png" height="96" width="auto" />
+          </div>
 				</div>
 			</div>
 		</div>
@@ -36,6 +79,13 @@
 
 <script lang="ts" setup>
 import imageLogo from "~/assets/image/logo-main.png";
+
+const otherLinks = [
+  { name: "Tentang Kami", link: "/tentang" },
+  { name: "Pameran & Kegiatan", link: "/pameran" },
+  { name: "Galeri", link: "/galeri" },
+  { name: "Hubungi Kami", link: "/hubungi" },
+];
 </script>
 
 <script lang="ts">
@@ -46,18 +96,17 @@ export default defineComponent({
 });
 </script>
 
-
 <style lang="scss" scoped>
 footer {
-	background: darken(#805231, 25%);
+	background: mix(#000, #805231, 85%);
 
 	.footer-top {
-		padding: 3rem 0;
+		padding: 6.25rem 0 5rem 0;
 	}
 
 	.footer-bottom {
 		padding: 2rem 0;
-		background: rgba(darken(#805231, 100%), 0.5);
+		background: rgba(lighten(#805231, 15%), 0.125);
 
 		a {
 			color: white;
